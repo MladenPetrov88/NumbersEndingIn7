07409fbbb234301cbbb9e7555b328875408f7da9
MladenPetrov88 <50301670+MladenPetrov88@users.noreply.github.com>
2019-06-18 10:37:05 +0300

    Add files via upload

---
+++ NumbersEndingIn7.java
@@ -0,0 +1,8 @@
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int num = 7; num <= 1000 ; num+= 10) {
+            System.out.println(num);
+        }
+    }
+}
